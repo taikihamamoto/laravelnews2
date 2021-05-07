d6bed404898bb1bea1e143229461fc951e12c346
taikihamamoto <taiki200316@icloud.com>
2021-05-07 22:59:06 +0900

    second commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 // 記事一覧画面を表示
+Route::get('/', "ArticleController@index")->name('home');
+// 記事の投稿
+Route::post('/', "ArticleController@exeStore")->name('Articlestore');
+// 記事の削除
+Route::post('/delete/{id}',"ArticleController@exeDelete")->name('Articledelete');
+// コメント一覧画面を表示
+Route::get('/comment/{id}', "CommentController@index")->name('Commentpage');
+// コメントの投稿
+Route::post('Commentpage', "CommentController@exeStore")->name('Commentstore');
+// コメントの削除
+Route::post('/comment/delete/{id}', "CommentController@exeDelete")->name('Commentdelete');
